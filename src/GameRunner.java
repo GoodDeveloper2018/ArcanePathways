52b8395af9b62afdcfcbc677d78eff8bc8da1f2c
Arsh <80232792+GoodDeveloper2018@users.noreply.github.com>
2024-11-11 23:48:30 -0500

    Update GameRunner.java
    Initialize first few aspects & arguments of GUI

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,10 +1,17 @@
-
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class GameRunner {
     public static void main(String[] args) {
-     String renderer = " ";
-     while (renderer != "Hello") {
-        System.out.println("Enter your UserName Combatant: ");
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(1800, 1000);
+        window.setResizable(false);
+        window.setTitle("Arcane Pathways");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
       }
     }
-}
