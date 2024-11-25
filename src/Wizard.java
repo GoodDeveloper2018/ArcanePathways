780f1a63b9fbe95be97d1911ce6ca8056804d2d1
Arsh <80232792+GoodDeveloper2018@users.noreply.github.com>
2024-11-25 07:05:38 -0500

    Update Wizard.java
    Addition of java docs and creation of attack method to support dueling functionality in game

--- src/Wizard.java
+++ src/Wizard.java
@@ -1,13 +1,29 @@
+/**
+ * The Wizard class represents a character archetype with high magical attributes.
+ */
 public class Wizard extends Character {
+    /**
+     * Constructor for the Wizard class.
+     *
+     * @param name the name of the wizard
+     */
     public Wizard(String name) {
         super(name, "Wizard");
     }
 
     @Override
     protected void initializeAttributes() {
-        this.health = 50;
-        this.strength = 3;
-        this.magic = 15;
+        this.health = 80;
+        this.strength = 5;
+        this.magic = 20;
         this.movementRange = 3; // Wizards can move 3 blocks per turn
+    }
+
+    @Override
+    public String attack(Character target) {
+        // Wizard casts a fireball
+        int damage = this.magic;
+        target.health -= damage;
+        return this.name + " casts a fireball at " + target.getName() + " for " + damage + " magical damage!";
     }
 }
