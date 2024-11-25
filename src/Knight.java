99321e1af0ca467cf57ed22ca8e9b749204a0f7f
Arsh <80232792+GoodDeveloper2018@users.noreply.github.com>
2024-11-25 07:05:14 -0500

    Update Knight.java
    Addition of java docs and creation of attack method to support dueling functionality in game

--- src/Knight.java
+++ src/Knight.java
@@ -1,13 +1,29 @@
+/**
+ * The Knight class represents a character archetype with balanced physical attributes.
+ */
 public class Knight extends Character {
+    /**
+     * Constructor for the Knight class.
+     *
+     * @param name the name of the knight
+     */
     public Knight(String name) {
         super(name, "Knight");
     }
 
     @Override
     protected void initializeAttributes() {
-        this.health = 90;
-        this.strength = 8;
-        this.magic = 3;
+        this.health = 120;
+        this.strength = 15;
+        this.magic = 5;
         this.movementRange = 2; // Knights can move 2 blocks per turn
+    }
+
+    @Override
+    public String attack(Character target) {
+        // Knight performs a sword slash
+        int damage = this.strength;
+        target.health -= damage;
+        return this.name + " slashes " + target.getName() + " with a mighty sword for " + damage + " damage!";
     }
 }
