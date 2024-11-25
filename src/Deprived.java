8d4ee76ef5fab570d76429edec82e927aaa287b6
Arsh <80232792+GoodDeveloper2018@users.noreply.github.com>
2024-11-25 07:04:50 -0500

    Update Deprived.java
    Addition of java docs and creation of attack method to support dueling functionality in game

--- src/Deprived.java
+++ src/Deprived.java
@@ -1,13 +1,42 @@
+import java.util.Random;
+
+/**
+ * The Deprived class represents a character archetype with balanced and flexible attributes.
+ */
 public class Deprived extends Character {
+    private Random random = new Random();
+
+    /**
+     * Constructor for the Deprived class.
+     *
+     * @param name the name of the deprived character
+     */
     public Deprived(String name) {
         super(name, "Deprived");
     }
 
     @Override
     protected void initializeAttributes() {
-        this.health = 70;
-        this.strength = 6;
-        this.magic = 6;
+        this.health = 100;
+        this.strength = 10;
+        this.magic = 10;
         this.movementRange = 4; // Deprived characters can move 4 blocks per turn
+    }
+
+    @Override
+    public String attack(Character target) {
+        // Deprived can choose between a basic attack or a magic attack
+        boolean useMagic = random.nextBoolean();
+        int damage;
+        String attackType;
+        if (useMagic) {
+            damage = this.magic;
+            attackType = "magical attack";
+        } else {
+            damage = this.strength;
+            attackType = "basic attack";
+        }
+        target.health -= damage;
+        return this.name + " performs a " + attackType + " on " + target.getName() + " for " + damage + " damage!";
     }
 }
