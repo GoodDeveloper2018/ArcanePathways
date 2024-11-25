1f62e5559c932397402ad8004a671cdf3effcdf9
Arsh <80232792+GoodDeveloper2018@users.noreply.github.com>
2024-11-25 07:02:47 -0500

    Update Enemey.java
    Inclusive of javadocs and added functionality for duel player intractability

--- src/Enemey.java
+++ src/Enemey.java
@@ -1,16 +1,42 @@
+import java.util.Random;
+
+/**
+ * The Enemy class represents an enemy character in the game.
+ */
 public class Enemy extends Character {
+    private Random random = new Random();
+
+    /**
+     * Constructor for the Enemy class.
+     *
+     * @param name the name of the enemy
+     */
     public Enemy(String name) {
         super(name, "Enemy");
     }
 
     @Override
     protected void initializeAttributes() {
-        this.health = 60; // Example stats
-        this.strength = 7;
-        this.magic = 4;
+        this.health = 100; // Example stats
+        this.strength = 10;
+        this.magic = 10;
+        this.movementRange = 2; // Enemies can move 2 blocks per turn
     }
 
-    public String getAttackType() {
-        return Math.random() > 0.5 ? "Physical" : "Magical";
+    @Override
+    public String attack(Character target) {
+        // Enemy randomly chooses between physical and magical attack
+        boolean useMagic = random.nextBoolean();
+        int damage;
+        String attackType;
+        if (useMagic) {
+            damage = this.magic;
+            attackType = "magical attack";
+        } else {
+            damage = this.strength;
+            attackType = "physical attack";
+        }
+        target.health -= damage;
+        return this.name + " uses a " + attackType + " on " + target.getName() + " for " + damage + " damage!";
     }
 }
